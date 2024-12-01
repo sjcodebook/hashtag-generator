f8e5addcbd4f7609b66fa72ed331f022773fd4bc
0xSJ <jainsanmati846@gmail.com>
2024-12-01 19:57:54 +0530

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+Hoot & Hustle by ProcrastiNix
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import CssBaseline from '@mui/material/CssBaseline'
+import { Box } from '@mui/material'
+
+export default function App() {
+  return (
+    <Box width='100%'>
+      <CssBaseline />
+      sdds
+    </Box>
+  )
+}
